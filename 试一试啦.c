df67f433e75ee0d090cd911f9b749ba26b556eac
YANG <120728598+QinyaYang@users.noreply.github.com>
2023-09-18 22:45:17 +0800

    add a file   初识C语言
    转义字符   while循环  if语句   函数简单应用(简化)  数组(用while实现）

--- 试一试啦.c
+++ 试一试啦.c
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>                 //引用库函数   standard input output
+#include<stdio.h> 
+#include<string.h>
+//引用库函数   standard input output
 
 //2023.9.16
 //int main()                        //int表示main函数调用返回一个整型值
@@ -168,15 +170,16 @@
 //	printf("TODAY=%d\n", TODAY);    //枚举常量——描述今天星期几
 //	return 0;
 //}
-int main()
-{                                    //character sring        字符串
-	//char string[] = { "abcde" }; 
-	char string[] = "abc" ;//后面跟有\0     \0——the ending of character string         
-	char arr[] = { 'a', 'b', 'c' };//后无结束标志，后打印随即值
-	printf("%s\n", string);         //  '\0'--0  'a'--97  'A'--65  ASCII编码
-	printf("%s\n", arr);
-	return 0;
-}
+//int main()
+//{                                    //character sring        字符串
+//	//char string[] = { "abcde" }; 
+//	char string[] = "abc" ; 后面跟有\0     \0——the ending of character string         
+//	char arr[] = { 'a', 'b', 'c' };      
+// 后无结束标志，后打印随即值
+//	printf("%s\n", string);           ASCII码 '\0'--0  'a'--97  'A'--65  
+//	printf("%s\n", arr);
+//	return 0;
+//}
 //int main()
 //{
 //	int a;
@@ -189,4 +192,167 @@ int main()
 //	printf("%d\n", a);
 //
 //	return 0;
+//}                      》》》》23-9-18《《《《
+//int main()
+//{
+//	int a;
+//	int b;                          review
+//	int c;
+//	int sum;
+//	scanf("%d%d%d", &a, &b, &c);
+//	sum = a + b + c;
+//	printf(" sum=%d\n ", sum);
+//	return 0;
 //}
+//int main()
+//{
+//	char arr1[] = { "abc" };
+//	char arr2[] = { 'a','b','c'};       //无'\0'，后面是随机值，strlen不可知
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}                  >>>>>>>>>转义字符<<<<<<<
+//int main()
+//{
+//	printf("%s\n","aabbcc\n");               // \n————换行
+//	return 0;
+//}
+//int main()
+//{                                     // \————表达原本含义，不被解析
+//	printf("%s","c:\\test\\32\\test.c");     //  \t————水平制表符      
+//	return 0;                          //\ddd————八进制数字
+//}
+////int main()
+//{
+//	printf("(are u ok???)\n");       <老版本>   ??)————被解析成三字母词
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%c\n", '\"');              // \————表示原有意思
+//	return 0;
+//}
+#include<string.h>
+/*int main()
+{
+	char arr[] = { "c.\test\32\test.c" };        method 1
+	printf("%d\n", strlen(arr));
+	return 0;
+}*/
+/*int main()
+{
+	printf("%d", strlen("c.\test\32\test.c"));   method  2
+	return 0;
+}*/
+//int main()
+//{
+//	printf("%c\n",'\x61');     //--->a
+//	return 0;
+//}                      >>>>>>>选择语句<<<<<<<<<
+/*int main()
+{
+	int choice = 0;
+	printf("加入比特\n");
+	printf("你要好好学习吗？\n (1 / 0) > :");
+	scanf("%d", &choice);
+	if (choice == 1)
+	{                                              //>>>>>>>if 语句
+		printf("good offer GET\n");
+	}
+	else
+		printf("回家烤红薯\n");
+	return 0;
+}*/
+/*int main()
+{
+	int line = 0;
+	printf("开始学习计算机\n");
+	while (line < 20000)
+	{
+		printf("继续努力敲代码:line=%d\n",line);   //>>>>>>>>>>>while语句
+		line++;
+	}
+	if (line >= 20000)
+		printf("离成功不远罗\n");
+	return 0;
+}*/
+/*int main()
+{
+	int num1;
+	int num2;
+	int sum;
+	printf("计算两个数加法:\n");      ???????   将其用自定义函数
+	scanf("%d%d", &num1, &num2);
+	sum = num1 + num2;
+	printf("num=%d\n", sum);
+	return 0;
+}*/
+/*int Add(int  x, int y)            >
+{                                   >
+	int z = x + y;                  > >>》自定义函数   两个数相加
+	return z;                       >   
+}                                   >
+int main()
+{
+	//int num1 = 10;
+	//int num2 = 20;
+	int sum = 0;
+	int a = 100;
+	int b = 200;
+	//sum = Add(num1, num2);
+	sum = Add(a, b);
+	printf("sum=%d\n", sum);
+	return 0;
+}*/
+/*int Multiply(int x, int y)           >
+{                                      >
+	int z = x * y;                     > >>>》用函数实现两个数相乘
+	return z;                          >  
+}                                      >
+int main()
+{
+	int num1 = 2;
+	int num2 = 4;
+	int mul = 0;
+	mul = Multiply(num1, num2);
+	printf("mul=%d\n", mul);
+	return 0;
+}*/           
+//               >>>>>>>>>>>>数组<<<<<<<<<<<
+//int main()
+//{
+//	int ch1[5] = { 1,2,3,4,5 };
+//	printf("%d\n", ch1[1]);
+//	printf("%d\n", ch1[1:4]);        //对数组的访问
+//
+//	return 0;
+//}
+/*int main()
+{
+	//char arr[5] = { 'a','f','s','g','t'};
+	//printf("%c\n", arr[2]);
+	//printf("%c\n", arr[-1]);  X error
+	//char arr[3] = { "add","egg","easy" };   X error
+	//printf("%s\n", arr[0]);
+
+	int arr[4] = { 1,2,3,4 };
+	int i = 0;
+	while (i < 4)
+	{
+		printf("%d\t", arr[i]);      //数组长度不是为常量的吗？？？？？？
+		i++;
+	}
+
+	return 0;
+}*/
+/*int main()
+{
+	int see[5] = { 3,5,6,2,6 };
+	int i = 0;
+	while (0 < i < 3)                     //print一个范围的怎么做？？？？？
+	{
+		printf("%d\t", see[i]);
+		i++;
+	}
+	return 0;
+}*/
