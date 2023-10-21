0a35be7e37b8cf0c324c64acde295228cd1083e4
YANG <120728598+QinyaYang@users.noreply.github.com>
2023-10-21 23:44:13 +0800

    working

--- workingcodetest.c
+++ workingcodetest.c
@@ -45,7 +45,7 @@
 //        return 0;
 //    
 //}
-// 1*1=1
+// 1*1=1                                        //  九九乘法表
 // 1*2=2 2*2=4
 // 1*3=3 2*3=6 3*3=9   ....
 // 1*9=9 2*9=18 3*9=29 4*9=36 ... 9*9=81
@@ -74,13 +74,67 @@
 //	printf("%c", '\b');
 //	return 0;
 //}
-int main()
-{
-	/*char a[] = { 0 };
-	scanf("%c",&a);
-	printf("%c++++\n", a);*/
-	char arr[] = { 0 };
-	scanf("%c", &arr);
-	printf("%c", arr);
+//int main()
+//{
+//	/*char a[] = { 0 };
+//	scanf("%c",&a);
+//	printf("%c++++\n", a);*/
+//	char arr[] = { 0 };
+//	scanf("%c", &arr);
+//	printf("%c", arr);
+//	return 0;
+//}
+//int main()                   //  交换两个数的情况
+//{
+//	int a = 1;
+//	int b = 2;
+//	int emp = a;
+//	a = b;
+//	b = emp;
+//	printf("%d %d", a, b);
+//
+//	return 0;
+//}
+
+/*int main()                  // 输入三个整数 并按从小到大的顺序排列
+{                                          //一一比大小 再交换
+	int a = 0;
+	int b = 0;
+	int c = 0;                              //input : 1 2 3
+	scanf("%d %d %d", &a, &b, &c);       // 原始顺序  a b c
+	if (a < b)
+	{
+		int emp = a;                     // b a c
+		a = b;
+		b = emp;
+	}
+	if (a < c)
+	{                                    // b c a
+		int emp = a;
+		a = c;
+		c = emp;
+	}
+	if (b < c)                          // c b a
+	{
+		int emp = b;
+		b = c;
+		c = emp;
+	}
+	
+	//if (a < b)                            
+	//{
+	//	a = b;      // a = b = 2
+	//	b = a;
+	//}
+	//if (a < c)
+	//	a = c;    // a = c = 3        XXXXXXXXXXXXXXXXXXXX  error 
+	//	c = a;                      
+	//}
+	//if (b < c)
+	//{
+	//	b = c;   // b = c = 3
+	//	c = b;
+	//}
+	printf("%d %d %d", a, b, c);
 	return 0;
-}
+}*/
